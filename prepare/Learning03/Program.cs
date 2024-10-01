d545334f5836345c3645c3c5c604a4a926921270
Your Name <you@example.com>
2024-10-01 16:44:54 +0800

    Learning Activity 3 Done

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -7,28 +7,28 @@ class Program
         Fraction f1 = new Fraction();
         Console.WriteLine("Fraction:");
         Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine("Decimal:");
+        Console.WriteLine("Decimal Value:");
         Console.WriteLine(f1.GetDecimalValue());
         Console.WriteLine();
 
         FractionTwo f2 = new FractionTwo();
         Console.WriteLine("Fraction:");
         Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine("Decimal:");
+        Console.WriteLine("Decimal Value:");
         Console.WriteLine(f2.GetDecimalValue());
         Console.WriteLine();
 
         FractionThree f3 = new FractionThree();
         Console.WriteLine("Fraction:");
         Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine("Decimal:");
+        Console.WriteLine("Decimal Value:");
         Console.WriteLine(f3.GetDecimalValue());
         Console.WriteLine();
 
         FractionFour f4 = new FractionFour();
         Console.WriteLine("Fraction:");
         Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine("Decimal:");
+        Console.WriteLine("Decimal Value:");
         Console.WriteLine(f4.GetDecimalValue());
 
     }
